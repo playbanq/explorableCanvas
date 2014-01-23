deccbca85c3b998a34868ed450a12e3ab70ad2fa
Jorge Zaccaro <jorgezaccaro@gmail.com>
2014-01-22 23:22:30 -0500

    Added the #drawMap method.

--- explorableCanvas.js
+++ explorableCanvas.js
@@ -1,24 +1,46 @@
 var ExplorableCanvas = function (id, width, height) {
-    var canvas, element = document.getElementById(id);
-    if (!element) {
+    var canvas, canvasElement = document.getElementById(id);
+    if (!canvasElement) {
         console.log('ERROR: The element with id "' + id + '" was not found in the DOM.');
         return
-    } else if (element.nodeName !== 'CANVAS') {
+    } else if (canvasElement.nodeName !== 'CANVAS') {
         console.log('ERROR: The element with id ' + id + ' is not a canvas element.');
         return;
     }
+    canvas = canvasElement;
+    var context = canvas.getContext('2d');
 
     var width = (typeof width === 'number') ? width : 300,
-        height = (typeof width === 'number') ? height : 150;
+        height = (typeof width === 'number') ? height : 150,
+        offsetX = 0,
+        offsetY = 0;
 
     var properties = {
         'setSize': {
             value: function (width, height) {
                 console.log(width, height);
-                element.width = width;
-                element.height = height;
+                canvasElement.width = width;
+                canvasElement.height = height;
             }
-        }
+        },
+        'drawMap': {
+            value: function (map) {
+                var top = 0, left = 0, withinCanvas, shiftedX, shiftedY;
+                context.font = '20px Sans';
+
+                map.elements.forEach(function (element) {
+                    shiftedX = element.x + offsetX;
+                    shiftedY = element.y + offsetY;
+
+                    withinCanvas = (shiftedX > 0 && shiftedX < canvasElement.width) &&
+                                    (shiftedY > 0 && shiftedX < canvasElement.height);
+
+                    if (withinCanvas) {
+                        context.fillText(element.data, element.x, element.y);
+                    }
+                });
+            }
+        } 
     };
 
     canvas = Object.create({}, properties);
