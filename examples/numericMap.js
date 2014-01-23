08b7ce3f52145914651aaef76e7e6927ff71b90c
Jorge Zaccaro <jorgezaccaro@gmail.com>
2014-01-22 23:24:13 -0500

    Changed the way the map elements are stored with their positions in the 'elements' array.

--- examples/numericMap.js
+++ examples/numericMap.js
@@ -1,22 +1,26 @@
+// Create numeric map
+var numericMap = {
+    tileSize: 20,
+    zoneSize: 10,
+    width: 30,
+    height: 30,
+    elements: []
+};
+
+var value, zoneSize = numericMap.zoneSize;
+for (var i = 0; i < numericMap.height; i++) {
+    for (var j = 0; j < numericMap.width; j++) {
+        value = (i % zoneSize === 0 || j % zoneSize === 0) ? '' : Math.floor(Math.random() * 10);
+        numericMap.elements.push({
+            x: i * numericMap.tileSize,
+            y: j * numericMap.tileSize,
+            data: value
+        });
+    }
+}
+
+// Create canvas and start drawing
 var canvas = new ExplorableCanvas('explorableCanvas');
 canvas.setSize(500, 500);
 
-var numericMap = {
-    width: 100,
-    height: 100,
-    values: []
-};
-
-for (var i = 0; i < numericMap.width; i++) {
-    numericMap.values[i] = [];
-    for (var j = 0; j < numericMap.width; j++) {
-        numericMap.values[i][j] = Math.floor(Math.random() * 10);
-    }
-}
-
-for (var i = 0; i < numericMap.width; i++) {
-    var line = '';
-    for (var j = 0; j < numericMap.width; j++) {
-        line += numericMap.values[i][j];
-    }
-}
+canvas.drawMap(numericMap);
