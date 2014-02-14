fc5e8032954bcfc172ff7fd31116bd2eb56e0eb2
Jorge Zaccaro <jorgezaccaro@gmail.com>
2014-02-14 18:00:24 -0500

    Modified the top, left, bottom and right properties to show grid extensibility.

--- examples/gridofZones.js
+++ examples/gridofZones.js
@@ -4,18 +4,20 @@ var canvas = document.getElementById('explorableCanvas'),
 TiledCanvas.extend(canvas, 50);
     canvas.setSize(500, 500);
     canvas.grid.setSize(1000, 1000);
+// The grid can be extended in all directions
+    canvas.grid.top -= 50;
+    canvas.grid.left -= 50;
+    canvas.grid.bottom += 50;
+    canvas.grid.right += 50;
 
 ExplorableCanvas.extend(canvas);
-    canvas.setSpeed({
-        x: 0.3,
-        y: 0.3
-    });
+    canvas.setSpeed({ x: 0.3, y: 0.3 });
 
 setInterval(function () {
     context.clearRect(0, 0, canvas.width, canvas.height);
     canvas.grid.draw();
-    for (var row = 0, rows = canvas.grid.rows; row <= rows; row++) {
-        for (var col = 0, columns = canvas.grid.columns; col <= columns; col++) {
+    for (var row = 0, rows = canvas.grid.rows; row < rows; row++) {
+        for (var col = 0, columns = canvas.grid.columns; col < columns; col++) {
             if (row % 2 === 0 && col % 2 === 1 || row % 2 === 1 && col % 2 === 0) {
                 canvas.grid.drawTile(row, col);
             }
